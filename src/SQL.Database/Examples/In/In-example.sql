ec33bc8478d104c6e2225ccd60d903314c90b603
Kiran <kiranjagz@gmai.com>
2023-04-24 22:07:46 +0200

    feature: added in statements

---
+++ src/SQL.Database/Examples/In/In-example.sql
@@ -0,0 +1,7 @@
+﻿SELECT *
+FROM Person.StateProvince
+WHERE StateProvinceCode IN ('AK','AZ','CO','ID','IA');
+ 
+SELECT *
+FROM Person.StateProvince
+WHERE StateProvinceCode NOT IN ('AK','AZ','CO','ID','IA');
