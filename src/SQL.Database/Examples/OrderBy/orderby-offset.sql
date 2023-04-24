e8f601a9640d06ab90b283691c332e7c2c8b2ccd
Kiran <kiranjagz@gmai.com>
2023-04-24 22:32:08 +0200

    feature: added group and order by

---
+++ src/SQL.Database/Examples/OrderBy/orderby-offset.sql
@@ -0,0 +1,15 @@
+﻿SELECT BusinessEntityID, NationalIDNumber, HireDate
+FROM HumanResources.Employee
+ORDER BY HireDate ASC
+OFFSET 5 ROWS
+FETCH NEXT 20 ROWS ONLY;
+ 
+SELECT BusinessEntityID, NationalIDNumber, HireDate
+FROM HumanResources.Employee
+ORDER BY HireDate DESC
+OFFSET 0 ROWS
+FETCH NEXT 5 ROWS ONLY;
+ 
+SELECT TOP 5 BusinessEntityID, NationalIDNumber, HireDate
+FROM HumanResources.Employee
+ORDER BY HireDate DESC;
