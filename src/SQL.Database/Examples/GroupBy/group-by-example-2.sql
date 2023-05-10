acaa719246d77c13ee64731c92f0a007abefae85
Kiran <kiranjagz@gmai.com>
2023-05-10 23:05:22 +0200

    refactor: code refactor in sql scripts

---
+++ src/SQL.Database/Examples/GroupBy/group-by-example-2.sql
@@ -0,0 +1,31 @@
+﻿SELECT *
+FROM HumanResources.EmployeePayHistory;
+ 
+SELECT PayFrequency, SUM(Rate)
+FROM HumanResources.EmployeePayHistory;
+ 
+SELECT PayFrequency, SUM(Rate) AS TotalRatePerPayFrequency
+FROM HumanResources.EmployeePayHistory
+GROUP BY PayFrequency;
+ 
+SELECT PayFrequency, SUM(Rate) AS TotalRatePerPayFrequency
+FROM HumanResources.EmployeePayHistory
+GROUP BY PayFrequency
+ORDER BY PayFrequency;
+ 
+SELECT PayFrequency, SUM(Rate) AS TotalRatePerPayFrequency
+FROM HumanResources.EmployeePayHistory
+GROUP BY PayFrequency
+ORDER BY PayFrequency DESC;
+ 
+SELECT ProductID, Shelf
+FROM Production.ProductInventory
+ 
+SELECT ProductID, Shelf, SUM(Quantity)
+FROM Production.ProductInventory
+GROUP BY ProductID, Shelf;
+ 
+SELECT ProductID, Shelf, SUM(Quantity) AS SumPerProductPerShelf
+FROM Production.ProductInventory
+GROUP BY ProductID, Shelf
+ORDER BY shelf DESC;
