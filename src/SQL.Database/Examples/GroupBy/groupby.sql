7365d385a4a53441afaf4b84d95d7cc857371cc7
Kiran <kiranjagz@gmai.com>
2023-05-10 23:02:55 +0200

    feat: added new sql scripts

--- src/SQL.Database/Examples/GroupBy/groupby.sql
+++ src/SQL.Database/Examples/GroupBy/groupby.sql
@@ -1,1 +1,14 @@
-﻿
+﻿SELECT *
+FROM HumanResources.Department;
+ 
+SELECT GroupName
+FROM HumanResources.Department
+GROUP BY GroupName;
+ 
+SELECT GroupName, COUNT(GroupName)
+FROM HumanResources.Department
+GROUP BY GroupName;
+ 
+SELECT GroupName, COUNT(*)
+FROM HumanResources.Department
+GROUP BY GroupName;
