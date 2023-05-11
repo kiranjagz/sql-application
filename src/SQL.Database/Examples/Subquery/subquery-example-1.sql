40814e81e1c8a87a066d71c96e4115c21228c3eb
Kiran <kiranjagz@gmai.com>
2023-05-11 22:23:24 +0200

    refactor: subquery example

--- src/SQL.Database/Examples/Subquery/subquery-example-1.sql
+++ src/SQL.Database/Examples/Subquery/subquery-example-1.sql
@@ -1,1 +1,16 @@
-﻿
+﻿SELECT BusinessEntityID,NationalIDNumber, JobTitle, HireDate
+FROM HumanResources.Employee
+WHERE BusinessEntityID IN
+   (SELECT BusinessEntityID
+     FROM HumanResources.EmployeeDepartmentHistory
+   )
+ORDER BY JobTitle;
+ 
+SELECT BusinessEntityID,NationalIDNumber, JobTitle, HireDate
+FROM HumanResources.Employee
+WHERE BusinessEntityID IN
+   (SELECT BusinessEntityID
+     FROM HumanResources.EmployeeDepartmentHistory
+	 WHERE BusinessEntityID <= 100
+   )
+ORDER BY JobTitle;
