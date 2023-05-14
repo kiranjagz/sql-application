ed0b445ba693fcf1bc40b1480f29f2cf041fd567
Kiran <kiranjagz@gmai.com>
2023-05-14 18:28:30 +0200

    feat: added escape and underscore

---
+++ src/SQL.Database/Examples/Like/underscore.sql
@@ -0,0 +1,12 @@
+﻿SELECT *
+FROM Person.StateProvince
+WHERE StateProvinceCode LIKE 'A[BKL]_';
+ 
+SELECT *
+FROM Person.StateProvince
+--WHERE StateProvinceCode LIKE 'A[BKL]_';
+WHERE Name LIKE 'AL____';
+ 
+SELECT PersonType
+FROM Person.Person
+WHERE PersonType LIKE 'S_';
