bca68aec115089fc8a1f5ca763b974f2b35ec1c8
Kiran <kiranjagz@gmai.com>
2023-05-30 19:22:45 +0200

    feat: added char example

--- src/SQL.Database/Examples/Strings/char-index.sql
+++ src/SQL.Database/Examples/Strings/char-index.sql
@@ -1,1 +1,12 @@
-﻿
+﻿SELECT CHARINDEX('D','DAD');
+SELECT CHARINDEX('D','DAD',2);
+ 
+SELECT Name
+FROM Production.Product
+ 
+SELECT Name, CHARINDEX('Cr', Name)
+FROM Production.Product;
+ 
+SELECT Name, CHARINDEX('Cr', Name)
+FROM Production.Product
+WHERE Name LIKE '%Cr%';
